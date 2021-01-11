8f061c642d4a776657f54cba8253fce4e257cd97
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-11 16:03:17 +0900

    Remove excess files

--- cache/home-2.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Page from "../pages/home-2";
-import props from "./props.generated.json";
-ReactDOM.hydrate(/* @__PURE__ */ React.createElement(Page, {
-  ...props["home-2"]
-}), document.getElementById("root"));
--- cache/home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Page from "../pages/home";
-import props from "./props.generated.json";
-ReactDOM.hydrate(/* @__PURE__ */ React.createElement(Page, {
-  ...props["home"]
-}), document.getElementById("root"));
