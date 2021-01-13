9d7c78c824603a1a713be88c0672500f865cb803
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-13 18:41:38 +0900

    Added internal folder as a demonstration of the ideal API

--- commands/conf.ts
+++ commands/conf.ts
@@ -1,7 +1,4 @@
 // Top-level configuration.
-//
-// TODO: Should be able to be user-generated. See Remix Run for further
-// inspiration.
 export default {
 	PAGES_DIR: "pages",
 	CACHE_DIR: "cache",
