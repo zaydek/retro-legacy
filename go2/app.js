0ed1dbce61bafbd02a7f7a17e01a9b9ad751aab2
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-18 23:47:17 +0900

    Better error-handling and added a static path resolver

--- go2/app.js
+++ go2/app.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOMServer from "react-dom/server"
 
 async function awaitRun() {
-	const { load, head: Head } = require("./retro-app/pages/index")
+	const { load, head: Head } = require("%RETRO_STATIC_PATH%")
 	const loadProps = await load()
 	const head = ReactDOMServer.renderToStaticMarkup(<Head {...loadProps} />)
 	// .replace(/\/>/g, " />") // /> -> ·/>
--- go2/main.go
+++ go2/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
+	"path"
 	"time"
 
 	"github.com/evanw/esbuild/pkg/api"
@@ -16,10 +16,42 @@ type ReactMeta struct {
 	Head      string                 `json:"head"`
 }
 
-// esbuild retro-app/pages/index.js \
-//   --bundle \
-//   --define:process.env.NODE_ENV=\"production\" \
-//   --loader:.js=jsx
+// func main() {
+//   result := api.Build(api.BuildOptions{
+//     EntryPoints: []string{"app.js"},
+//     Bundle:      true,
+//     Outfile:     "out.js",
+//     Incremental: true,
+//   })
+//   if len(result.Errors) > 0 {
+//     os.Exit(1)
+//   }
+//
+//   // Call "Rebuild" as many times as you want
+//   for i := 0; i < 5; i++ {
+//     result2 := result.Rebuild()
+//     if len(result2.Errors) > 0 {
+//       os.Exit(1)
+//     }
+//   }
+// }
+
+// func IPCReactLoadPropsAndHead() ReactMeta
+
+var resolveRetroStaticPathPlugin = api.Plugin{
+	Name: "resolveRetroStaticPathPlugin",
+	Setup: func(build api.PluginBuild) {
+		build.OnResolve(api.OnResolveOptions{Filter: "^%RETRO_STATIC_PATH%$"},
+			func(args api.OnResolveArgs) (api.OnResolveResult, error) {
+				cwd, err := os.Getwd()
+				if err != nil {
+					return api.OnResolveResult{}, fmt.Errorf("cannot cwd for plugin resolveRetroStaticPathPlugin; %w", err)
+				}
+				// Must resolve an absolute path; use cwd:
+				return api.OnResolveResult{Path: path.Join(cwd, "retro-app/pages/index.js")}, nil
+			})
+	},
+}
 
 func main() {
 	t1 := time.Now()
@@ -28,11 +60,11 @@ func main() {
 		Define:      map[string]string{"process.env.NODE_ENV": `"production"`},
 		EntryPoints: []string{"app.js"},
 		Loader:      map[string]api.Loader{".js": api.LoaderJSX},
-		Outfile:     "tmp.js",
+		Plugins:     []api.Plugin{resolveRetroStaticPathPlugin},
 	})
 	if len(result.Errors) > 0 {
 		bstr, _ := json.MarshalIndent(result.Errors, "", "\t")
-		fmt.Fprintln(os.Stderr, bstr)
+		fmt.Fprintln(os.Stderr, string(bstr))
 		os.Exit(1)
 	}
 	fmt.Printf("%0.3fs - esbuild\n", time.Since(t1).Seconds())
@@ -41,7 +73,7 @@ func main() {
 	cmd := exec.Command("node")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		log.Fatalln(fmt.Errorf("failed to pipe stdin: %w", err))
+		panic(fmt.Errorf("failed to pipe stdin: %w", err))
 	}
 
 	go func() {
@@ -51,14 +83,17 @@ func main() {
 
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalln(fmt.Errorf("failed to run command: %w", err))
+		if len(out) != 0 { // stderr takes precedence
+			panic(fmt.Errorf("failed to run command: %s", out))
+		}
+		panic(fmt.Errorf("failed to run command: %w", err))
 	}
 	fmt.Printf("%0.3fs - node\n", time.Since(t2).Seconds())
 
 	var meta ReactMeta
 	err = json.Unmarshal(out, &meta)
 	if err != nil {
-		log.Fatalln(fmt.Errorf("failed to unmarshal: %w", err))
+		panic(fmt.Errorf("failed to unmarshal: %w", err))
 	}
 
 	fmt.Print("loadProps=", meta.LoadProps, "\n")
