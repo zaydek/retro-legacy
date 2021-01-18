7bb560a9fe5dbb78ff232e032e04feede00fefe9
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-18 23:53:24 +0900

    Extracted a pure function that takes a filename and resoles ReactMeta

--- go2/main.go
+++ go2/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path"
-	"time"
 
 	"github.com/evanw/esbuild/pkg/api"
 )
@@ -16,64 +16,40 @@ type ReactMeta struct {
 	Head      string                 `json:"head"`
 }
 
-// func main() {
-//   result := api.Build(api.BuildOptions{
-//     EntryPoints: []string{"app.js"},
-//     Bundle:      true,
-//     Outfile:     "out.js",
-//     Incremental: true,
-//   })
-//   if len(result.Errors) > 0 {
-//     os.Exit(1)
-//   }
-//
-//   // Call "Rebuild" as many times as you want
-//   for i := 0; i < 5; i++ {
-//     result2 := result.Rebuild()
-//     if len(result2.Errors) > 0 {
-//       os.Exit(1)
-//     }
-//   }
-// }
+func resolveMeta(filename string) (ReactMeta, error) {
+	resolveRetroStaticPathPlugin := api.Plugin{
+		Name: "resolveRetroStaticPathPlugin",
+		Setup: func(build api.PluginBuild) {
+			build.OnResolve(api.OnResolveOptions{Filter: "^%RETRO_STATIC_PATH%$"},
+				func(args api.OnResolveArgs) (api.OnResolveResult, error) {
+					cwd, err := os.Getwd()
+					if err != nil {
+						return api.OnResolveResult{}, fmt.Errorf("cannot cwd for plugin resolveRetroStaticPathPlugin; %w", err)
+					}
+					return api.OnResolveResult{Path: path.Join(cwd, filename)}, nil
+				})
+		},
+	}
 
-// func IPCReactLoadPropsAndHead() ReactMeta
-
-var resolveRetroStaticPathPlugin = api.Plugin{
-	Name: "resolveRetroStaticPathPlugin",
-	Setup: func(build api.PluginBuild) {
-		build.OnResolve(api.OnResolveOptions{Filter: "^%RETRO_STATIC_PATH%$"},
-			func(args api.OnResolveArgs) (api.OnResolveResult, error) {
-				cwd, err := os.Getwd()
-				if err != nil {
-					return api.OnResolveResult{}, fmt.Errorf("cannot cwd for plugin resolveRetroStaticPathPlugin; %w", err)
-				}
-				// Must resolve an absolute path; use cwd:
-				return api.OnResolveResult{Path: path.Join(cwd, "retro-app/pages/index.js")}, nil
-			})
-	},
-}
-
-func main() {
-	t1 := time.Now()
 	result := api.Build(api.BuildOptions{
 		Bundle:      true,
-		Define:      map[string]string{"process.env.NODE_ENV": `"production"`},
+		Define:      map[string]string{"process.env.NODE_ENV": "\"production\""},
 		EntryPoints: []string{"app.js"},
 		Loader:      map[string]api.Loader{".js": api.LoaderJSX},
 		Plugins:     []api.Plugin{resolveRetroStaticPathPlugin},
 	})
 	if len(result.Errors) > 0 {
-		bstr, _ := json.MarshalIndent(result.Errors, "", "\t")
-		fmt.Fprintln(os.Stderr, string(bstr))
-		os.Exit(1)
+		bstr, err := json.MarshalIndent(result.Errors, "", "\t")
+		if err != nil {
+			return ReactMeta{}, fmt.Errorf("failed to marshal; %w", err)
+		}
+		return ReactMeta{}, errors.New(string(bstr))
 	}
-	fmt.Printf("%0.3fs - esbuild\n", time.Since(t1).Seconds())
 
-	t2 := time.Now()
 	cmd := exec.Command("node")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		panic(fmt.Errorf("failed to pipe stdin: %w", err))
+		return ReactMeta{}, fmt.Errorf("failed to pipe stdin: %w", err)
 	}
 
 	go func() {
@@ -84,18 +60,24 @@ func main() {
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		if len(out) != 0 { // stderr takes precedence
-			panic(fmt.Errorf("failed to run command: %s", out))
+			return ReactMeta{}, fmt.Errorf("failed to run command: %s", out)
 		}
-		panic(fmt.Errorf("failed to run command: %w", err))
+		return ReactMeta{}, fmt.Errorf("failed to run command: %w", err)
 	}
-	fmt.Printf("%0.3fs - node\n", time.Since(t2).Seconds())
 
 	var meta ReactMeta
 	err = json.Unmarshal(out, &meta)
 	if err != nil {
-		panic(fmt.Errorf("failed to unmarshal: %w", err))
+		return ReactMeta{}, fmt.Errorf("failed to unmarshal: %w", err)
 	}
+	return meta, nil
+}
 
+func main() {
+	meta, err := resolveMeta("retro-app/pages/index.js")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Print("loadProps=", meta.LoadProps, "\n")
 	fmt.Print("head=", meta.Head, "\n")
 }
