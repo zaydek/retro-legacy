ecfd8b0ee31b35c9c28737d691c26deb1fafc6b2
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-19 00:00:29 +0900

    Experimenting with concurrent builds; let’s try using Node for this instead

--- go2/app.js
+++ go2/app.js
@@ -5,8 +5,6 @@ async function awaitRun() {
 	const { load, head: Head } = require("%RETRO_STATIC_PATH%")
 	const loadProps = await load()
 	const head = ReactDOMServer.renderToStaticMarkup(<Head {...loadProps} />)
-	// .replace(/\/>/g, " />") // /> -> ·/>
-	// .replace(/></g, ">\n<") // >< -> >\n<
 	console.log(JSON.stringify({ loadProps, head }))
 }
 
