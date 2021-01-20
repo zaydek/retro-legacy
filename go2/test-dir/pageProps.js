12af9be7277d81a7b2a4554df15754e7d790222f
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-21 04:28:40 +0900

    Page props are resolving again

---
+++ go2/test-dir/pageProps.js
@@ -0,0 +1,12 @@
+// AUTO-GENERATED; DO NOT EDIT.
+
+module.exports = {
+  "PageIndex": {
+    "title": "Hello, world!",
+    "description": "This page was made using Retro."
+  },
+  "PageNested__Index": {
+    "title": "Nested hello, world!",
+    "description": "This page was made using Retro."
+  }
+}
