b861511ad01ce0747ec7c5fa37bc3df1e72d0707
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-27 04:16:27 +0900

    Cleaned up runtime.Build

--- go2/cmd/cmd_build.go
+++ go2/cmd/cmd_build.go
@@ -1,21 +1,8 @@
 package main
 
-import (
-	"os"
-)
-
-func (app *RetroApp) build() {
-	var err error
-	if app.Configuration, err = initConfiguration(); err != nil {
-		stderr.Println(err)
-		os.Exit(1)
-	} else if app.PageBasedRouter, err = initPageBasedRouter(app.Configuration); err != nil {
-		stderr.Println(err)
-		os.Exit(1)
-	}
-
-	must(copyAssetDirectoryToBuildDirectory(app))
-	must(prerenderProps(app))
-	must(prerenderApp(app))
-	must(prerenderPages(app))
+func (r Runtime) Build() {
+	must(copyAssetDirectoryToBuildDirectory(r.Config))
+	must(r.prerenderPropsImpl())
+	must(r.prerenderAppImpl())
+	must(r.prerenderPagesImpl())
 }
