9b548536389757575a1f6c4415358913b74c4608
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-29 09:04:12 +0900

    New package_test.go

---
+++ npm/retro-server/go/embeds/package_test.go
@@ -0,0 +1,41 @@
+package embeds
+
+import (
+	"bytes"
+	"testing"
+)
+
+const want = `{
+	"name": "hello-world",
+	"scripts": {
+		"watch": "retro-server watch",
+		"build": "retro-server build",
+		"serve": "retro-server serve"
+	},
+	"dependencies": {
+		"react": "^1.33.7",
+		"react-dom": "^1.33.7",
+		"retro-client": "^1.33.7",
+		"retro-server": "^1.33.7"
+	}
+}
+`
+
+func TestTemplate(t *testing.T) {
+	dot := PackageDot{
+		RepoName:           "hello-world",
+		ReactVersion:       "^1.33.7",
+		ReactDOMVersion:    "^1.33.7",
+		RetroClientVersion: "^1.33.7",
+		RetroServerVersion: "^1.33.7",
+	}
+
+	var buf bytes.Buffer
+	if err := PackageTemplate.Execute(&buf, dot); err != nil {
+		t.Fatal(err)
+	}
+
+	if got := buf.String(); got != want {
+		t.Fatalf("got:\n\n%s\n\nwant:\n\n%s", got, want)
+	}
+}
