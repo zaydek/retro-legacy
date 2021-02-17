b0345bbecedd50984e8be07c59d8216d0a736b6a
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-02-17 09:28:46 +0900

    Save changes to recover MVP code

---
+++ idea/pages/pikachu.js
@@ -0,0 +1,19 @@
+const css = scss`
+	div {
+		hello: world!
+	}
+
+	div {
+		hello: world!
+	}
+
+	// ...
+	// ...
+`
+
+// prettier-ignore
+export function App() {
+	return (
+		<div>Hello, world!</div>
+	)
+}
