c6f5b22639802fe0843b3c9f92ea4da725ed59d8
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-02-17 12:39:07 +0900

    Upgraded to a service-based architecture

--- go/render-pages.js
+++ go/render-pages.js
@@ -1,37 +1,49 @@
-const esbuild = require("esbuild")
-const fs = require("fs/promises")
-const path = require("path")
-const React = require("react")
-const ReactDOMServer = require("react-dom/server")
+// 	const p = new Promise(async resolve => {
+// 		const component = await prerenderComponent(service)(runtime, each)
+// 		const page = await prerenderPage(runtime, component)
+// 		resolve({ ...each, page })
+// 	})
+// 	promises.push(p)
+// }
+//
+// const arr = await Promise.all(promises)
+// const map = arr.reduce((acc, each) => {
+// 	acc[each.path] = each
+// 	return acc
+// }, {})
 
 async function run(runtime) {
-	for (const route of runtime.page_based_router) {
-		const src = route.src_path
-		const dst = path.join(runtime.dir_config.cache_dir, src.replace(/\.(jsx?|tsx?)$/, ".esbuild.$1"))
+	const esbuild = require("esbuild")
+	const fs = require("fs/promises")
+	const path = require("path")
+	const React = require("react")
+	const ReactDOMServer = require("react-dom/server")
 
-		try {
-			await esbuild.build({
+	const service = await esbuild.startService()
+
+	try {
+		// TODO: Upgrade to Promise.all (maybe add --concurrent?).
+		for (const route of runtime.page_based_router) {
+			const src = route.src_path
+			const dst = path.join(runtime.dir_config.cache_dir, src.replace(/\.(jsx?|tsx?)$/, ".esbuild.$1"))
+
+			await service.build({
 				bundle: true,
 				define: {
 					__DEV__: true,
 					"process.env.NODE_ENV": JSON.stringify("development"),
 				},
-				format: "cjs",
 				entryPoints: [src],
 				format: "cjs",
-				outfile: dst,
 				loader: {
 					".js": "jsx",
 				},
-				logLevel: "silent",
+				logLevel: "silent", // TODO
+				outfile: dst,
+				// plugins: [...configs.retro.plugins],
 			})
-		} catch (err) {
-			console.log(err.message)
-			return
-		}
+			// TODO: Handle warnings and hints.
 
-		let html
-		try {
 			const mod = require("./" + dst)
 
 			let head = "<!-- <Head> -->"
@@ -41,19 +53,19 @@ async function run(runtime) {
 			const page = ReactDOMServer.renderToString(React.createElement(mod.default))
 
 			// prettier-ignore
-			html = runtime.base_page
+			const html = runtime.base_page
 				.replace("%head%", head)
 					.replace(/></g, ">\n\t\t<")
 					.replace(/\/>/g, " />")
 				.replace("%page%", `<noscript>You need to enable JavaScript to run this app.</noscript>
 		<div id="react-root">${page}</div>
 		<script src="/app.js"></script>`)
-		} catch (err) {
-			console.log(err.message)
-			return
-		}
 
-		await fs.writeFile(route.dst_path, html)
+			await fs.writeFile(route.dst_path, html)
+		}
+	} catch (err) {
+		console.log(err.message)
+		process.exit(1)
 	}
 }
 
